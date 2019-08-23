e470b6644e412e45c27de9a97395ce0ab88c3d03
Unknown <Luther.ninja@gmail.com>
2019-08-23 09:10:09 -0600

    started Unity Project

---
+++ ProjectPointyHat/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController CC;
+
+    public float speed = 6.0f;
+
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CC.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"),0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        CC.Move(moveDirection * Time.deltaTime);
+    }
+}
