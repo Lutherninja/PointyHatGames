555d96a36c1f4fb43b11ff059deda7a87629bbe6
Unknown <Luther.ninja@gmail.com>
2019-09-14 22:37:18 -0600

    tried to get jump working by myself didn't quite work

--- SpaceDudes/Assets/Scripts/PlayerMovement.cs
+++ SpaceDudes/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     float horizontalMove = 0f;
 
@@ -17,10 +18,12 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if (Input.GetButtonDown("Crouch"))
         {
@@ -38,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
         //Move Character
         controller.Move(horizontalMove * Time.deltaTime, crouch, jump);
         jump = false;
+        animator.SetBool("IsJumping", false);
 
     }
 }
