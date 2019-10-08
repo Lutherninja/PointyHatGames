69d9fe0c617039f0829069a0dbe3864910738873
Jonathan Luther <luther.ninja@gmail.com>
2019-10-08 15:25:59 -0600

    started a project for the topdown game
    Got events set up for the rock pickup

---
+++ TopDownMessin/Assets/Scripts/ThrowableObjects/PickUpObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+   // public GameObject pickupVol;
+    public bool isUp;
+    public bool canPick;
+    public bool pickedUp;
+    public bool thrown;
+
+
+    public void Start()
+    {
+        pickedUp = false;
+        canPick = false;
+        thrown = false;
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (canPick && Input.GetKeyDown("y"))
+        {
+            pickedUp = true;
+        }
+
+        else if (pickedUp && Input.GetKeyDown("y"))
+        {
+            thrown = true;
+            pickedUp = false;
+        }
+
+    }
+
+    public void PlayerisinPickUpZone()
+    {
+        canPick = true;
+    }
+
+    public void PlayerLeftZone()
+    {
+        
+        canPick = false;
+    }
+}
