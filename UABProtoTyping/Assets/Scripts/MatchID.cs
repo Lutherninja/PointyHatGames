3655c829639f6c60acc684a6f4c216955924cff6
Jonathan Luther <luther.ninja@gmail.com>
2019-11-14 15:06:54 -0700

    Got stupid medicined to work

---
+++ UABProtoTyping/Assets/Scripts/MatchID.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchID : MonoBehaviour
+{
+    public UnityEvent OnMatch, NoMatch;
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print("collided");
+        
+        if (gameObject.tag == other.tag)
+        {
+            print("it worked");
+            OnMatch.Invoke();
+        }
+        else
+        {
+            NoMatch.Invoke();
+        }
+    }
+}
